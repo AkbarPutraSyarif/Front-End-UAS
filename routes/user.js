604cd8ec0d892eefdf32315bde93f8ad2c4864b9
AkbarPutraSyarif <akbarsyarif2.bang@gmail.com>
2024-11-21 07:34:55 +0700

    about caroussel, profile page, search.

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
-const User = require('../model/user.js'); // Import the User model
+const User = require('../model/user.js');
 const router = express.Router();
 
 // Registration API
